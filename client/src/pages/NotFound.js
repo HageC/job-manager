f48272eb0eea83eef2a736178b71829f13b53cb3
Christopher Hage <hage.chris@yahoo.com>
2022-10-05 17:54:54 -0300

    style change

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -4,7 +4,7 @@ import notfound from "../assets/not-found.svg";
 import { Link } from "react-router-dom";
 const StyleWrapper = styled.div.attrs({ className: "error-main" })`
   img {
-    margin: 5rem auto 0 auto;
+    margin: 10rem auto 0 auto;
   }
   h1 {
     margin: 2rem auto 0 auto;
