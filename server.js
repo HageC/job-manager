1527de4de2521365aafc526b9c5c04b4b905cd25
Christopher Hage <hage.chris@yahoo.com>
2022-10-06 18:13:40 -0300

    setup mongodb connection

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 import express from "express";
 import notFound from "./middleware/not-found.js";
 import errorHandler from "./middleware/error-handler.js";
+import mongoose from "mongoose";
+import "dotenv/config";
+import { start } from "repl";
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(notFound);
 app.use(errorHandler);
-app.listen(port, () => console.log(`Server is listening on port ${port}`));
+
+const startUp = async () => {
+  try {
+    mongoose.connect(process.env.MONGO_URL);
+    app.listen(port, () => console.log(`Server is listening on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startUp();
