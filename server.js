b6393cb53dcae20834ee59761e2c2f8249e84225
Christopher Hage <hage.chris@yahoo.com>
2022-10-06 18:03:52 -0300

    custom error class created and middleware created

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from "express";
-
+import notFound from "./middleware/not-found.js";
+import errorHandler from "./middleware/error-handler.js";
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(notFound);
+app.use(errorHandler);
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
