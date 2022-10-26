d92086855fd12cf3a93bf2e548b640f33dad94a5
Christopher Hage <hage.chris@yahoo.com>
2022-10-25 21:37:15 -0300

    monthly stats chart added

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,6 +1,7 @@
+import ChartStats from "./ChartStats";
 import Job from "./Job";
 import Jobs from "./Jobs";
 import Logo from "./Logo";
 import Notification from "./Notification";
 
-export { Jobs, Job, Logo, Notification };
+export { Jobs, Job, Logo, Notification, ChartStats };
